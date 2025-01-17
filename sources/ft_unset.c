96a2c2287623a4ae0f201eb099c0a40a243b98ea
Adrien Isidore <aisidore@bess-f4r4s9.clusters.42paris.fr>
2025-01-17 14:59:08 +0100

    v3 jan 17

--- sources/ft_unset.c
+++ sources/ft_unset.c
@@ -17,40 +17,53 @@
 
 //tranformer env en liste chainee. remove le noeud name (si y'en a pas on fait rien). je retransforme en envv et je remplace l'ancien env.
 
-void    ft_unset(char **env, char **argv)
-{//la boucle current doit etre enrobee dans la boucle argv car il faut le faire pour chaque argv a partir de cmds[1]
-    t_env *head;
-    t_env *current;
-    t_env *previous;
-    t_env *temp;//
-    int    i;
+int     ft_initunset(t_env **ptr_head, char **env, char **argv)
+{
+    int i;
 
     if (!argv[1])
-        return ;
-    head = NULL;
+        return (1);
+    *ptr_head = NULL;
     i = -1;
     while (env[++i])
-        ft_adder(&head, env[i]);
-    current = head;
-    previous = NULL;
-    i = 1;//on commence a cmds[1]
-    while (argv[i])
+        ft_adder(ptr_head, env[i]);
+    return (0);
+}
+
+void ft_freeun(t_env **current, t_env **previous, t_env **head)
+{
+    t_env *temp;
+    
+    temp = *current;
+    if (*previous) 
+        (*previous)->next = (*current)->next;
+    else 
+        *head = (*current)->next;
+    *current = (*current)->next;
+    free(temp->name);
+    free(temp->key);
+    free(temp);
+}
+
+void ft_unset(char **env, char **argv)
+{
+    t_env   *head;
+    t_env   *current;
+    t_env   *previous;
+    int     i;
+
+    if (ft_initunset(&head, env, argv))
+        return ;
+    i = 1; // On commence à cmd[1]
+    while (argv[i]) 
     {
-        while (current)
+        current = head;
+        previous = NULL;
+        while (current) 
         {
-            if (strcmp(current->name, argv[i]) == 0)
-            {
-                temp = current->next;
-                if (previous)
-                    previous->next = current->next;
-                else
-                    head = temp;
-                free(current->name);
-                free(current->key);
-                free(current);
-                current = temp;
-            }
-            else
+            if (strcmp(current->name, argv[i]) == 0) 
+                ft_freeun(&current, &previous, &head);
+            else 
             {
                 previous = current;
                 current = current->next;
@@ -59,11 +72,11 @@ void    ft_unset(char **env, char **argv)
         i++;
     }
     //a la fin on modifie env et on free l'ancienne version !!!!!!!
-    //(Ici on le fait pas car env est de la stack et c le vrai env)
+    //ft_unset peut retourner un char **array comme ca dans ft_builtin on fera juste env = ft_unset
     ft_printexport(head);
     ft_freelexport(head);
-    return (ft_printexport(head), ft_freelexport(head));
 }
+
 
 //gcc -o ft_unset sources/env_manager.c sources/ft_tokenize.c sources/parsing.c sources/ft_concat.c sources/str_manager.c sources/libft_a.c sources/libft_abis.c sources/ft_export_utils.c sources/ft_split_utils.c sources/ft_split.c sources/ft_ambig.c sources/ft_getenvv.c sources/ft_ifexpand.c sources/ft_export.c sources/ft_unset.c
 //valgrind --leak-check=full ./ft_unset "bonjour=\"ok=ok\""

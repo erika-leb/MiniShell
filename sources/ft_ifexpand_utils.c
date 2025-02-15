89fad1bf2876ac122ad2d32deca7165429bdab1f
Adrien Isidore <aisidore@made-f0Br6s1.clusters.42paris.fr>
2025-02-15 16:16:00 +0100

    correction 2

--- sources/ft_ifexpand_utils.c
+++ sources/ft_ifexpand_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ifexpand_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ele-borg <ele-borg@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aisidore <aisidore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 18:23:36 by ele-borg          #+#    #+#             */
-/*   Updated: 2025/02/14 18:47:17 by ele-borg         ###   ########.fr       */
+/*   Updated: 2025/02/15 16:15:48 by aisidore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_initev(t_forenvv *ev, char *res, t_element *elements, t_gc *gc)
 {
-	ev->result = ft_strdup_(res, gc);
+	(void) gc;
+	ev->result = res;
 	ev->elements = elements;
 }
